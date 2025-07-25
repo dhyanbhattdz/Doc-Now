ead2ac9a0acc450e5fc571332bf1183db3762002
dhyanbhattdz <148819370+dhyanbhattdz@users.noreply.github.com>
2025-07-25 21:24:06 +0530

    Update AppContext.jsx

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -6,7 +6,7 @@ export const AppContext = createContext()
 
 const AppContextProvider = (props) => {
     const currencySymbol = '₹'
-    const backendUrl = "https://doc-now.onrender.com"
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
 
     const [doctors, setDoctors] = useState([])
     const [token, setToken] = useState(localStorage.getItem('token') || '')
